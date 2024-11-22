3332b3d6501d3fc29fcff87e774c42a0e57e3e75
frantisekpetko <oristgrey@gmail.com>
2024-11-22 14:04:24 +0100

    [IMP] Deployment update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     server: {
-        port: 4000
+        port: 4000,
     },
     preview: {
-        port: 5000
+        port: 5000,
     },
-    plugins: [
-        react()
-
-    ],
+    plugins: [react()],
     resolve: {
         alias: [{ find: 'src', replacement: '/src' }],
     },
-
-})
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '', // optional: global imports
+                includePaths: [path.resolve(__dirname, 'node_modules')],
+            },
+        },
+    },
+});
